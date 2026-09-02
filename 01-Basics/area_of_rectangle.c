55858bddc18464ce13743a068f5feba10451d19d
roushankumar07 <roushankr.dev@gmail.com>
2026-09-02 20:01:58 +0530

    Add area of rectangle program

---
+++ 01-Basics/area_of_rectangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+    int main(){
+        int length , breadth;
+        printf("What is the length of rectange is\n");
+        scanf("%d",&length);
+
+        printf("What is the breadth of rectangle is\n");
+        scanf("%d",&breadth);
+
+        printf("The area of rectangle is %d", length*breadth);
+
+        return 0;
+
+    }
