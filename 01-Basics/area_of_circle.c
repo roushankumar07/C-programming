3078c3084c42d9152422842b3f0d663a894398e0
roushankumar07 <roushankr.dev@gmail.com>
2026-09-02 20:17:37 +0530

    Add

---
+++ 01-Basics/area_of_circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+    int main(){
+
+        int radius = 3;
+        float pi = 3.14;
+        printf("The area of this circle is %f\n",pi*radius*radius);
+        
+        return 0;
+    }
